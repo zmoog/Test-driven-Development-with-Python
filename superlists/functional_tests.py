a54ca9fe688004af941fa42654d651a19c8752df
Maurizio Branca <maurizio.branca@gmail.com>
2014-07-09 07:14:31 +0200

    Updated functional test
    Added a few step in the user story

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -4,4 +4,13 @@ browser = webdriver.Firefox()
 
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the page title and header mention to-do lists
+assert 'To-DO' in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# ...
+
+# Satisfied, she goes back to sleep
+
+browser.quit()

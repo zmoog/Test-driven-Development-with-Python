9deeea462dcfbadf87f2df5c6557030f59062bcf
Maurizio Branca <maurizio.branca@gmail.com>
2014-11-02 17:11:52 +0100

    Refactored FT code into a helper method.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_later(self):
 
         self.browser.get('http://localhost:8000')
@@ -28,27 +33,24 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        # ..
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        
+        self.check_for_row_in_list_table('1: Buy peacock feathers') 
 
         # There is still a text box inviting her to add another item. She enters
-        # "Use peacock feathers to make a fly" (Edith is very methodical.
+        # "Use peacock feathers to make a fly" (Edith is very methodical).
 
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
- 
+        # The pages updates again, and now shows both item in her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers') 
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly') 
+
         # ...
         
         # Satisfied, she goes back to sleep
